97269d1146f6c6e3840d95609b8b686cb153baab
Tanawat Sitthitan <TanawatSitthitan@192.168.1.3>
2018-03-15 20:54:14 +0700

    commit project to github

---
+++ resources/views/login.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    {{--include bootstrap css--}}
+
+    {{--include bootstrap js--}}
+
+
+    <link href="{!! asset('css/bootstrap-4.0.0/dist/css/bootstrap.min.css') !!}" media="all" rel="stylesheet" type="text/css" />
+    <script type="text/javascript" src="{!! asset('css/bootstrap-4.0.0/dist/css/bootstrap.min.js') !!}"></script>
+
+    <title>Laravel</title>
+</head>
+<body>
+<div class="container">
+    <h1 class="">Hi</h1>
+</div>
+</body>
+</html>
